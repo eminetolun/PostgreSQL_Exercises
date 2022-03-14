311e60ed676e84f8a4c76f193bf43dc0502e56cf
Emine Tolun <84921349+eminetolun@users.noreply.github.com>
2022-03-14 12:57:15 +0300

    Update between-in.sql

--- between-in.sql
+++ between-in.sql
@@ -6,9 +6,9 @@ WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 -- actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralanması
 
 SELECT first_name, last_name FROM actor
-where first_name IN('Penelope', 'Nick', 'Ed');
+WHERE first_name IN('Penelope', 'Nick', 'Ed');
 
 -- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralanması
 
 SELECT * FROM film
-where rental_rate IN (0.99 , 2.99 , 4.99) AND replacement_cost IN (12.99 , 15.99 , 28.99);
+WHERE rental_rate IN (0.99 , 2.99 , 4.99) AND replacement_cost IN (12.99 , 15.99 , 28.99);
