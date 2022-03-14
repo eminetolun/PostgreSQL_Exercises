5971f82f981f9a727369935a3157c6cddcd7054b
Emine Tolun <84921349+eminetolun@users.noreply.github.com>
2022-03-14 17:24:42 +0300

    Create subquery.sql

---
+++ subquery.sql
@@ -0,0 +1,23 @@
+-- film tablosunda uzunluğu, ortalama film uzunluğundan fazla kaç tane film vardır?
+
+SELECT COUNT(*) FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+-- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+-- film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+
+SELECT title FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)  AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+
+-- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+SELECT payment.customer_id, first_name, last_name, COUNT(payment.customer_id) AS payment_count FROM payment
+LEFT JOIN customer ON payment.customer_id = customer.customer_id
+GROUP BY payment.customer_id, customer.first_name, customer.last_name
+ORDER BY payment_count DESC;
+
