3529e31e535ac0c3da8b2fa0b3a65748058da99d
Emine Tolun <84921349+eminetolun@users.noreply.github.com>
2022-03-14 12:55:48 +0300

    Update like-ilike.sql

--- like-ilike.sql
+++ like-ilike.sql
@@ -1,22 +1,22 @@
  -- country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananların sıralanması
  
- select country from country
- where country ~~ 'A%a'; 
+ SELECT country FROM country
+ WHERE country ~~ 'A%a'; 
  
 -- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananların sıralanması
 
-select country from country
-where country ~~ '______n%' ;
+SELECT country FROM country
+WHERE country ~~ '______n%' ;
 
 -- film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerinin sıralanması
 
-select title from film
-where title ~~* '%T%T%T%T%';
+SELECT title FROM film
+WHERE title ~~* '%T%T%T%T%';
 
 
 -- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verilerin sıralanması
 
-select * from film
-where (title ~~ 'C%') and (length > 90) and (rental_rate = 2.99);
+SELECT * FROM film
+WHERE (title ~~ 'C%') AND (length > 90) AND (rental_rate = 2.99);
 
 
